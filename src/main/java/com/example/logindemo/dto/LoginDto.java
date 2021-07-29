e384058809dffc4bbb169b8eff051458fcf90991
huangrenhao <1355345165@qq.com>
2021-07-29 15:51:33 +0800

    DTO to Dto

---
+++ src/main/java/com/example/logindemo/dto/LoginDto.java
@@ -0,0 +1,18 @@
+package com.example.logindemo.dto;
+
+import com.alibaba.fastjson.annotation.JSONType;
+import lombok.Data;
+
+/**
+ * @author hrh13
+ * @date 2021/7/16
+ */
+@Data
+@JSONType(orders = {"id","account","passWord","gmtCreate"})
+public class LoginDto {
+    private Integer id;
+    private String account;
+    private String passWord;
+    private long gmtCreate;
+
+}
---
+++ src/main/java/com/example/logindemo/dto/UpdateDto.java
@@ -0,0 +1,13 @@
+package com.example.logindemo.dto;
+
+import lombok.Data;
+
+/**
+ * @author hrh13
+ * @date 2021/7/16
+ */
+@Data
+public class UpdateDto {
+
+    private String passWord;
+}
