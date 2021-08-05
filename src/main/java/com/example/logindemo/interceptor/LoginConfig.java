95a96bfd9c05fa718e4f411a894c5cdf8a2b6024
huangrenhao <1355345165@qq.com>
2021-08-05 17:36:23 +0800

    拦截器调用service

--- src/main/java/com/example/logindemo/interceptor/LoginConfig.java
+++ src/main/java/com/example/logindemo/interceptor/LoginConfig.java
@@ -1,5 +1,7 @@
 package com.example.logindemo.interceptor;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -11,10 +13,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class LoginConfig implements WebMvcConfigurer {
+    @Autowired
+    LoginHandlerInterceptor loginHandlerInterceptor;
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         //注册TestInterceptor拦截器
-        InterceptorRegistration registration = registry.addInterceptor(new LoginHandlerInterceptor());
+        InterceptorRegistration registration = registry.addInterceptor(loginHandlerInterceptor);
         registration.addPathPatterns("/**");
         registration.excludePathPatterns("/users/login", "/users", "/employees", "/employees/login");
     }
