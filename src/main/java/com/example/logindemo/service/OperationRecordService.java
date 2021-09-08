3c765e018aecf7d8597a8cc654908dc39ceda2c4
huangrenhao <1355345165@qq.com>
2021-09-08 16:10:16 +0800

    简单修改

--- src/main/java/com/example/logindemo/service/OperationRecordService.java
+++ src/main/java/com/example/logindemo/service/OperationRecordService.java
@@ -3,6 +3,7 @@ package com.example.logindemo.service;
 import com.alibaba.fastjson.JSON;
 import com.example.logindemo.dao.OperationRecordDao;
 import com.example.logindemo.entity.OperationRecordEntity;
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.Signature;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,32 +21,25 @@ import static com.example.logindemo.dto.ConstantValue.SUCCEED;
  * @date 2021/9/8
  */
 @Service
+@Slf4j
 public class OperationRecordService {
     @Autowired
     private OperationRecordDao operationRecordDao;
 
     public void createRecord(JoinPoint joinPoint, Object returnValue) {
-        // 获取签名
-        Signature signature = joinPoint.getSignature();
-        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        Signature signature=joinPoint.getSignature();
+        ServletRequestAttributes attributes= (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         assert attributes != null;
-        HttpServletRequest request = attributes.getRequest();
+        HttpServletRequest request= attributes.getRequest();
         String jsonString = JSON.toJSONString(joinPoint.getArgs());
-        //传入参数
         String args = jsonString.substring(1, jsonString.length() - 1);
-        // 获取切入的包名
-        String declaringTypeName = signature.getDeclaringTypeName();
-        // 获取即将执行的方法名
-        String funcName = signature.getName();
-        // 获取请求 URL
-        String url = request.getRequestURL().toString();
 
         OperationRecordEntity recordEntity = new OperationRecordEntity();
         recordEntity.setOperator(request.getHeader(HEADER_EMPLOYEE_ID));
         recordEntity.setParameter(args);
-        recordEntity.setUrl(url);
-        recordEntity.setPacketName(declaringTypeName);
-        recordEntity.setFuncName(funcName);
+        recordEntity.setUrl(request.getRequestURI());
+        recordEntity.setPacketName(signature.getDeclaringTypeName());
+        recordEntity.setFuncName(signature.getName());
         recordEntity.setGmtCreate(System.currentTimeMillis());
         if (returnValue != null) {
             recordEntity.setResult(SUCCEED);
--- src/test/java/com/example/logindemo/LoginDemoApplicationTests.java
+++ src/test/java/com/example/logindemo/LoginDemoApplicationTests.java
@@ -1,19 +1,11 @@
 package com.example.logindemo;
 
-import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@Slf4j
 class LoginDemoApplicationTests {
     @Test
     public void contextLoads() {
-        log.trace("这是trace");
-        log.debug("这是debug");
-        log.info("这是info");
-        log.warn("这是warn");
-        log.error("这是error");
     }
-
 }
